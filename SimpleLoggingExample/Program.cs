cdd5f2b099097fc05fd9d85e93b4e0baf8c7f0cb
CodingBeagle <kasper.rie@gmail.com>
2026-02-07 17:07:42 +0100

    fixed using statements

--- SimpleLoggingExample/Program.cs
+++ SimpleLoggingExample/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Extensions.Logging;
+using System;
 
 // Configure Serilog with the console sink
 Log.Logger = new LoggerConfiguration()
